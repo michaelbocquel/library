e5ee24a0fc1cc59af3ae6782ad48167f22ba80e9
nosca <michael.bocquel@gmail.com>
2024-02-12 19:27:50 +0100

    created some instances of books - added them to the container for some visualisation - deleted genre in book constructor because too vague

--- script.js
+++ script.js
@@ -1,15 +1,40 @@
-const myLibrary = []
-console.log(myLibrary)
+// Initialise empty library
 
-function Book(title, author, genre, pages, isRead) {
+const myLibrary = []
+
+function Book(title, author, pages, isRead) {
 	this.title = title
 	this.author = author
-	this.genre = genre
 	this.pages = pages
 	this.isRead = isRead
 }
 
+const bookContainer = document.querySelector('.book-container')
 const newBookButton = document.querySelector('.new-book-button')
 const newBookForm = document.querySelector('.new-book-form')
 
+
+
 newBookButton.addEventListener('click', () => { newBookForm.showModal() })
+
+// Book Objects
+
+const riveGauche = new Book('Rive Gauche', 'Pierre Bordage', 601, true)
+const riveDroite = new Book('Rive Droite', 'Pierre Bordage', 567, true)
+myLibrary.push(riveGauche, riveDroite)
+
+console.log(myLibrary)
+
+// add Books to the container
+
+function createBook() {
+	for (let i = 0; i < myLibrary.length; i++) {
+		const bookCard = document.createElement('div')
+		bookCard.style.width = '150px'
+		bookCard.style.height = '230px'
+		bookCard.style.backgroundColor = 'red'
+		bookContainer.insertBefore(bookCard, newBookButton)
+	}
+}
+createBook()
+
